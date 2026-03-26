699f8037d8dd5e25b62b2c33312d3731412c18dc
Md Nahidul Islam <183149325+wnzid@users.noreply.github.com>
2026-03-26 22:23:30 +0200

    initial

---
+++ clustering.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans, AgglomerativeClustering
+from sklearn.metrics import adjusted_rand_score, silhouette_score
